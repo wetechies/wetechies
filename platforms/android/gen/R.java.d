3b781d8be90371962478028edc370eccfab76184
nagashayan <nagashayan@engineer.com>
2013-07-31 13:46:16 +0530

    5

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+D:\wetechies\wetechies\platforms\android\gen\wetechies\blogspot\in\R.java \
+ : D:\wetechies\wetechies\platforms\android\res\drawable\icon.png \
+D:\wetechies\wetechies\platforms\android\res\drawable-hdpi\icon.png \
+D:\wetechies\wetechies\platforms\android\res\drawable-hdpi\ic_launcher.png \
+D:\wetechies\wetechies\platforms\android\res\drawable-ldpi\icon.png \
+D:\wetechies\wetechies\platforms\android\res\drawable-ldpi\ic_launcher.png \
+D:\wetechies\wetechies\platforms\android\res\drawable-mdpi\icon.png \
+D:\wetechies\wetechies\platforms\android\res\drawable-mdpi\ic_launcher.png \
+D:\wetechies\wetechies\platforms\android\res\drawable-xhdpi\icon.png \
+D:\wetechies\wetechies\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+D:\wetechies\wetechies\platforms\android\res\layout\main.xml \
+D:\wetechies\wetechies\platforms\android\res\values\strings.xml \
+D:\wetechies\wetechies\platforms\android\res\xml\config.xml \
+D:\wetechies\wetechies\platforms\android\bin\AndroidManifest.xml \
---
+++ platforms/android/gen/wetechies/blogspot/in/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package wetechies.blogspot.in;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
